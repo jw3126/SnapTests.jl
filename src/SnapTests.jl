0855ee55f8162384063515adf78a62479360dc9a
Jan Weidner <jw3126@gmail.com>
2023-06-03 20:37:10 +0200

    git commit -am init

--- src/SnapTests.jl
+++ src/SnapTests.jl
@@ -1,5 +1,101 @@
 module SnapTests
+using ArgCheck
 
-# Write your package code here.
+function matchsnap(snap, value; kw...)
+    cmp = cmpfun(snap, value)
+    matchsnap(cmp, snap, value; kw...)
+end
+
+function matchsnap(cmp, snap, value; 
+        on_snap_does_not_exist=:save,
+        on_cmp_error=:error,
+        on_cmp_false=:return,
+        on_load_error=:error,
+    )
+    @argcheck on_load_error in (:error, :replace)
+    @argcheck on_cmp_error in (:error, :replace)
+    @argcheck on_cmp_false in (:ask, :replace, :return)
+    @argcheck on_snap_does_not_exist in (:save, :error)
+
+    snap_value = if exists(snap, value)
+        try
+            load(snap, value)
+        catch err
+            if on_load_error === :error
+                rethrow()
+            elseif on_load_error === :replace
+                @info("Replacing snap after load error",
+                    error=err,
+                )
+                save(snap, value)
+                load(snap, value)
+            end
+        end
+    elseif on_snap_does_not_exist === :error
+        error("$snap does not exist.")
+    elseif on_snap_does_not_exist === :ask
+        error("TODO")
+    elseif on_snap_does_not_exist === :save
+        @info "$snap does not exist, saveing $value"
+        save(snap, value)
+        load(snap, value) # sanity check that reload works
+    else
+        error("Unreachable")
+    end
+    ispass = false
+    try
+        ispass = cmp(snap_value, value)::Bool
+    catch err
+        if on_cmp_error === :replace
+            # TODO diff
+            @info("Replacing snap after cmp error",
+                error=err,
+            )
+            save(snap, value)
+            snap_value = load(snap, value)
+            ispass = cmp(snap_value, value)::Bool
+        elseif on_cmp_error === :error
+            rethrow()
+        else
+            error("Unreachable")
+        end
+    end
+    @assert ispass isa Bool
+
+    if (!ispass) 
+        if on_cmp_false === :return
+            return ispass
+        elseif on_cmp_false === :ask
+            error("TODO")
+        elseif on_cmp_false === :replace
+            @info("Replacing snap after false comparison",
+            )
+            save(snap, value)
+            snap_value = load(snap, value)
+            ispass = cmp(snap_value, value)::Bool
+        end
+    end
+    return ispass
+end
+
+function exists(snap::AbstractString, value)
+    if isfile(snap)
+        return true
+    elseif ispath(snap)
+        error("$snap is a path but not a file.")
+    else
+        return false
+    end
+end
+function save(snap::AbstractString, value)
+    mkpath(dirname(snap))
+    write(snap, value)
+end
+function load(snap::AbstractString, value)
+    read(snap, typeof(value))
+end
+function cmpfun(snap, value)
+    isequal
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,92 @@
 using SnapTests
 using Test
+using SnapTests: matchsnap
+
+function testpath(args...)
+    joinpath(@__DIR__, args...)
+end
 
 @testset "SnapTests.jl" begin
-    # Write your tests here.
+    dir = mktempdir()
+
+    path1 = joinpath(dir, "snap1.txt")
+    value1 = "value1"
+    path2 = joinpath(dir, "snap2.txt")
+    value2 = "value2"
+
+    @test !ispath(path1)
+    @test !ispath(path2)
+    @test matchsnap(path1, value1)
+    @test isfile(path1)
+    @test matchsnap(path2, value2, on_snap_does_not_exist=:save)
+    @test isfile(path2)
+    @test matchsnap(path1, value1)
+    @test !matchsnap(path1, value2)
+    @test matchsnap(Returns(true), path1, value2)
+    @test !matchsnap(Returns(false), path1, value1)
+    @test matchsnap(path2, value2)
+    @test matchsnap(path1, value1, on_snap_does_not_exist=:error)
+    @test !matchsnap(path1, value2, on_snap_does_not_exist=:error)
+    @test_throws "on_snap_does_not_exist in" matchsnap(path1, value1, on_snap_does_not_exist=:nonsense)
+    @test_throws "on_cmp_error in" matchsnap(path1, value1, on_cmp_error=:nonsense)
+    @test_throws "on_cmp_false in" matchsnap(path1, value1, on_cmp_false=:nonsense)
+    # @test_throws ArgumentError 
+end
+
+struct Lookup
+    dict
+    key
+end
+function SnapTests.load(l::Lookup, value)
+    l.dict[l.key]
+end
+function SnapTests.save(l::Lookup, value)
+    l.dict[l.key] = value
+end
+function SnapTests.exists(l::Lookup, value)
+    haskey(l.dict, l.key)
+end
+
+@testset "Customization" begin
+    d = Dict()
+    @test matchsnap(Lookup(d, :key1), 1)
+    @test d == Dict(:key1 => 1)
+    @test !matchsnap(Lookup(d, :key1), 2)
+    @test d == Dict(:key1 => 1)
+    @test matchsnap(Lookup(d, :key1), 1, on_snap_does_not_exist=:error)
+    @test matchsnap(Lookup(d, :key1), 1, on_snap_does_not_exist=:error, on_cmp_false=:replace)
+    @test d == Dict(:key1 => 1)
+    @test matchsnap(Lookup(d, :key2), 2)
+    @test d == Dict(:key1 => 1, :key2 => 2)
+    @test matchsnap(Lookup(d, :key2), 42, on_cmp_false=:replace)
+    @test d == Dict(:key1 => 1, :key2 => 42)
+end
+
+mutable struct DB
+    getindex
+    haskey
+    setindex!
+end
+
+Base.getindex(db::DB, key) = db.getindex(key)
+Base.haskey(db::DB, key) = db.haskey(key)
+Base.setindex!(db::DB, val, key) = db.setindex!(val, key)
+function DB(dict::AbstractDict)
+    getindex = key -> dict[key]
+    haskey = key -> Base.haskey(dict, key)
+    setindex! = (val, key) -> dict[key] = val
+    DB(getindex, haskey, setindex!)
+end
+
+@testset "on load error" begin
+    dict = Dict()
+    db = DB(dict)
+    @test matchsnap(Lookup(db, 1), 10)
+    @test matchsnap(Lookup(db, 1), 10)
+    db.haskey = Returns(true)
+    @test matchsnap(Lookup(db, 1), 10)
+    @test dict == Dict(1 => 10)
+    @test_throws KeyError matchsnap(Lookup(db, 2), 10)
+    @test matchsnap(Lookup(db, 2), 10, on_load_error=:replace)
+    @test dict == Dict(1 => 10, 2=>10)
 end
